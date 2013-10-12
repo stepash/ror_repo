f9d07fde532c3bdd5690895c0789f7c66b0682cb
SPShoup <spshoup@gmail.com>
2013-10-12 23:15:02 +1100

    This is the initial Commit Massage

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '7107741faf8aca793c1f0db0e92b892d69d7271c052998b6b6f64208dda24834115358b8073cf96fbdaa6e543ef7a53dd7e823f2c9c84e995df8d01359d34932'
